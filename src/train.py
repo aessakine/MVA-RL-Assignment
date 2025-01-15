fc671824d37dd91b5a177b400aee14aca9d0f8cb
AmirEssakine <amer.essakine@ens-paris-saclay.fr>
2025-01-15 13:17:17 +0100

    train.py

--- src/train.py
+++ src/train.py
@@ -35,7 +35,7 @@ class ReplayBuffer:
         self.index = (self.index + 1) % self.capacity
     def sample(self, batch_size):
         batch = random.sample(self.data, batch_size)
-        return list(map(lambda x:torch.Tensor(np.array(x)).to(self.), list(zip(*batch))))
+        return list(map(lambda x:torch.Tensor(np.array(x)).to(self.device), list(zip(*batch))))
     def __len__(self):
         return len(self.data)
 
