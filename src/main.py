f59c7af6fdc5bc7be0bb3098f31a6b9062929992
AmirEssakine <amer.essakine@ens-paris-saclay.fr>
2025-01-15 23:28:36 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
         seed_everything(seed=42)
         # Initialization of the agent. Replace DummyAgent with your custom agent implementation.
         agent = ProjectAgent()
-        agent.load('best_hiv_agent (2).pth')
+        agent.load('best_score (5).pth')
         # Evaluate agent and write score.
         score_agent: float = evaluate_HIV(agent=agent, nb_episode=5)
         score_agent_dr: float = evaluate_HIV_population(agent=agent, nb_episode=20)
